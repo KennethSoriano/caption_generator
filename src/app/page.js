9752a8d8d1f1530a31c390379a97404869eed77c
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-19 18:48:20 -0800

    AWS setup and file upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import DemoSection from "@/components/DemoSection";
 import PageHeaders from "@/components/PageHeaders";
 import SparklesIcon from "@/components/SparklesIcon";
+import UploadForm from "@/components/UploadForm";
 import UploadIcon from "@/components/UploadIcon";
 
 
 export default function Home() {
+  function upload(ev) {
+    ev.preventDefault();
+    console.log(ev);
+  }
   return (
     <>
       <PageHeaders 
@@ -12,10 +17,7 @@ export default function Home() {
         h2Tect={'Just Upload your video and we will do the rest'}
       />
       <div className="text-center">
-        <button className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50">
-          <UploadIcon />
-          Choose File
-        </button>
+        <UploadForm />
       </div>
       <DemoSection />
     </>
