ed597e17e94a7a1a32068c159741875c5d2dad6f
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-19 17:36:37 -0800

    home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import SparklesIcon from '@/components/SparklesIcon'
+
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +15,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gradient-to-b from-bg-gradient-from to-bg-gradient-to text-white`}>
-        {children}
+        <main className="p-4 max-w-2xl mx-auto">
+        <header className="flex justify-between my-8">
+          <Link href="/" className="flex gap-1 ">
+            <SparklesIcon />
+            <span>EpicCaptions</span>
+          </Link>
+          <nav className="flex gap-6 text-white/80">
+            <Link href="/">Home</Link>
+            <Link href="/pricing">Pricing</Link>
+            <Link href="/mailto:contact">Contact</Link>
+          </nav>
+        </header>
+          {children}
+        </main>
       </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,23 @@
+import DemoSection from "@/components/DemoSection";
+import PageHeaders from "@/components/PageHeaders";
 import SparklesIcon from "@/components/SparklesIcon";
+import UploadIcon from "@/components/UploadIcon";
 
 
 export default function Home() {
   return (
-    <main className="p-4 max-w-2xl mx-auto">
-      <header className="flex justify-between my-8">
-        <a href="" className="flex gap-1 ">
-          EpicCaptions
-        <span>EpicCaptions</span>
-        </a>
-        <nav className="flex gap-6 text-white/80">
-          <a href="">Home</a>
-          <a href="">Pricing</a>
-          <a href="">Contact</a>
-        </nav>
-      </header>
-      <div className="text-center mt-24">
-        <h1 className="text-3xl" style={{textShadow: '1px 1px 0 rgba(0,0,0,.3'}}>
-          Add captions to your videos
-        </h1>
-        <h2 className="text-white/75">
-          Just upload your video and we will do the rest.
-        </h2>
-      </div>
+    <>
+      <PageHeaders 
+        h1Text={'Add captions to your videos'} 
+        h2Tect={'Just Upload your video and we will do the rest'}
+      />
       <div className="text-center">
         <button className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50">
+          <UploadIcon />
           Choose File
         </button>
       </div>
-      <div className="flex justify-around mt-12 items-center">
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"></div>
-        <div>
-          <SparklesIcon />
-        </div>
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"></div>
-      </div>
-    </main>
+      <DemoSection />
+    </>
   )
 }
