f1462ba850dd27cd61f390479bcb0fa3884dd316
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-20 09:27:44 -0800

    transcribe page

--- src/app/[filename]/page.js
+++ src/app/[filename]/page.js
@@ -1,13 +1,48 @@
 'use client'
-import axios from "axios"
-import { useEffect } from "react"
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 
 export default function FilePage({params}) {
-    const filename = params.filename
+    const filename = params.filename;
+    const [isTranscribing, setIsTranscribing] = useState(false);
+
+
     useEffect(() => {
-        axios.get('/api/transcribe?filename='+filename)
-    }, [filename])
+        getTranscription();
+    }, [filename]);
+
+    function getTranscription() {
+        setIsFetchingInfo(true);
+        axios.get('/api/transcribe?filename='+filename).then(response => {
+          setIsFetchingInfo(false);
+          const status = response.data?.status;
+          const transcription = response.data?.transcription;
+          if (status === 'IN_PROGRESS') {
+            setIsTranscribing(true);
+            setTimeout(getTranscription, 3000);
+          } else {
+            setIsTranscribing(false);
+    
+            setAwsTranscriptionItems(
+              clearTranscriptionItems(transcription.results.items)
+            );
+          }
+        });
+      }
+    
+      if (isTranscribing) {
+        return (
+          <div>Transcribing your video...</div>
+        );
+      }
+    
+      if (isFetchingInfo) {
+        return (
+          <div>Fetching information...</div>
+        );
+      }
+    
     return (
         <div>{filename}</div>
     )
